2c8eff83e9df893a26ec8e70105b04259a8a2214
Alejandro-bonafos <alejandro.bonafos@gmail.com>
2025-10-29 20:38:50 -0300

    correciones json

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   base: '/reclamos-front/',
+   base: '/reclamos-municipales-react/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
